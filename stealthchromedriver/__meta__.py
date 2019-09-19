e711352e17264dbe7e01fed19e227187b5eaf8f8
Leon <ultrafunkamsterdam@users.noreply.github.com>
2019-09-19 14:44:44 +0200

    Create __meta__.py

---
+++ stealthchromedriver/__meta__.py
@@ -0,0 +1,10 @@
+__title__ = "stealthchromedriver"
+__version__ = "1.3"
+__author__ = "UltrafunkAmsterdam"
+__license__ = "MIT"
+
+__description__ = "Optimized Selenium Chrome webdriver replacement which PASSES browser automation tests like Distill Network"
+
+__long_description__ = ("A stealthy Selenium Chrome webdriver replacement which does not trigger browser automation tests [date:08-2019] "
+                        "The API is fully compatible with regular selenium chromedriver"
+                        "Using this Chromedriver implementation you can easily bypass current automation detection algorithms like Distill")
